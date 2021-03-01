3e49f20b6305dda3d5cb3fcab691e2780c10802e
Gregory <gregory.delfrate@gmail.com>
2021-03-01 20:32:51 +0100

    create gh-pages

---
+++ build/precache-manifest.921be804eab7f490754d3581dca75437.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767e8c02f631b50bcdce426406a6a004",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca7d7629f77b7facd40e",
+    "url": "/static/css/main.e3a1b487.chunk.css"
+  },
+  {
+    "revision": "104ab31a3dc135d8fc62",
+    "url": "/static/js/2.05e84f7b.chunk.js"
+  },
+  {
+    "revision": "ca7d7629f77b7facd40e",
+    "url": "/static/js/main.412648ee.chunk.js"
+  },
+  {
+    "revision": "29e5325d0da269e00da3",
+    "url": "/static/js/runtime-main.db97b1d3.js"
+  },
+  {
+    "revision": "9073db8aec149e69c2502e04ad52d600",
+    "url": "/static/media/Kropy_Draach.9073db8a.png"
+  },
+  {
+    "revision": "475481389bb82e49234b88bfa7d590c5",
+    "url": "/static/media/Kropy_pilly.47548138.png"
+  },
+  {
+    "revision": "ec28c48ae34c13ff4fd8641f83ef17bd",
+    "url": "/static/media/MelKropyBocal.ec28c48a.png"
+  },
+  {
+    "revision": "32ab89f7d99c78cea6554d0f6b172854",
+    "url": "/static/media/MelKropyJeanLoup.32ab89f7.png"
+  },
+  {
+    "revision": "bd025f392aa8e7184186963fd5a2d322",
+    "url": "/static/media/logo_header.bd025f39.jpg"
+  },
+  {
+    "revision": "ed87d9974e09d0557089ede891647eca",
+    "url": "/static/media/space.ed87d997.jpg"
+  }
+]);

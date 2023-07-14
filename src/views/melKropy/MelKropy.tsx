825fbe93607b92533db40765d4b4f3679f9487b0
Gregory <gregory.delfrate@gmail.com>
2023-07-14 23:36:22 +0200

    add images

--- src/views/melKropy/MelKropy.tsx
+++ src/views/melKropy/MelKropy.tsx
@@ -1,42 +1,106 @@
 import React from 'react'
-import { Dialog, DialogContent, DialogContentText, DialogTitle, Grid, IconButton, ImageList, ImageListItem, Toolbar, useMediaQuery, useTheme } from '@mui/material'
+import {
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+  IconButton,
+  ImageList,
+  ImageListItem,
+  Toolbar,
+  useMediaQuery,
+  useTheme
+} from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
 import melKropyMenu from '../../img/rollup_mathieu_page-0001.jpg'
 import melKropyVisuel from '../../img/Visuel_Mug-Mel_&_Kropy.jpg'
-import melBioExpression from '../../img/mel_bio_expression.PNG'
-import kropyBioExpression from '../../img/kropy_bio_expression.PNG'
-import krikKrakBioExpression from '../../img/krik_krak_bio_expression.PNG'
-import jeanLoupBioExpression from '../../img/jean_loup_bio_expression.PNG'
-import ardinjBioExpression from '../../img/ardinj_bio_expression.PNG'
-import djinnoBioExpression from '../../img/djinno_bio_expression.PNG'
-import lorelaBioExpression from '../../img/lorela_bio_expression.PNG'
+import melBioExpression from '../../img/mel_bio_expression.png'
+import kropyBioExpression from '../../img/kropy_bio_expression.png'
+import krikKrakBioExpression from '../../img/krik_krak_bio_expression.png'
+import jeanLoupBioExpression from '../../img/jean_loup_bio_expression.png'
+import ardinjBioExpression from '../../img/ardinj_bio_expression.png'
+import djinnoBioExpression from '../../img/djinno_bio_expression.png'
+import lorelaBioExpression from '../../img/lorela_bio_expression.png'
+import carillonneurBioExpression from '../../img/Carillonneur.png'
+import drelinhoBioExpression from '../../img/Drelinho.png'
+import grousstbuchBioExpression from '../../img/Grousstbuch.png'
+import goldengoldBioExpression from '../../img/Goldengold.png'
+import holzernefloteBioExpression from '../../img/Holzerneflote.png'
+import jangDraachBioExpression from '../../img/Jang&Draach.png'
+import lesPillyBioExpression from '../../img/Les_Pilly.png'
+import mlleCrochueBioExpression from '../../img/MlleCrochue.png'
+import mllePAILLETTEBioExpression from '../../img/MllePAILLETTE.png'
 
 interface protagonistsDescType {
   img: string
   title: string
 }
-const images: protagonistsDescType[] = [{
-  img: kropyBioExpression,
-  title: 'Kropy'
-}, {
-  img: melBioExpression,
-  title: 'Mel'
-}, {
-  img: krikKrakBioExpression,
-  title: 'Krik Krak'
-}, {
-  img: jeanLoupBioExpression,
-  title: 'Kropy'
-}, {
-  img: ardinjBioExpression,
-  title: 'Kropy'
-}, {
-  img: djinnoBioExpression,
-  title: 'Kropy'
-}, {
-  img: lorelaBioExpression,
-  title: 'Kropy'
-}
+const images: protagonistsDescType[] = [
+  {
+    img: kropyBioExpression,
+    title: 'Kropy'
+  },
+  {
+    img: melBioExpression,
+    title: 'Mel'
+  },
+  {
+    img: krikKrakBioExpression,
+    title: 'Krik Krak'
+  },
+  {
+    img: jeanLoupBioExpression,
+    title: 'Jean Loup'
+  },
+  {
+    img: ardinjBioExpression,
+    title: 'Ardinj'
+  },
+  {
+    img: djinnoBioExpression,
+    title: 'Djinno'
+  },
+  {
+    img: lorelaBioExpression,
+    title: 'Lorela'
+  },
+  {
+    img: jangDraachBioExpression,
+    title: 'Jang & Draach'
+  },
+  {
+    img: lesPillyBioExpression,
+    title: 'Les Pilly'
+  },
+  {
+    img: drelinhoBioExpression,
+    title: 'Drelinho'
+  },
+  {
+    img: grousstbuchBioExpression,
+    title: 'GrousstBuch'
+  },
+  {
+    img: mlleCrochueBioExpression,
+    title: 'Mlle Crochue'
+  },
+  {
+    img: mllePAILLETTEBioExpression,
+    title: 'Mlle Paillette'
+  },
+  {
+    img: holzernefloteBioExpression,
+    title: 'HolzerneFlote'
+  },
+  {
+    img: goldengoldBioExpression,
+    title: 'GoldenGold'
+  },
+  {
+    img: carillonneurBioExpression,
+    title: 'Carillonneur'
+  }
 ]
 
 const MelKropy = (): JSX.Element => {
@@ -56,16 +120,30 @@ const MelKropy = (): JSX.Element => {
     <>
       <Grid container spacing={2} mt={2}>
         <Grid item xs={2}>
-            <img src={melKropyMenu} style={{ width: '100%', filter: 'drop-shadow( 15px 11px 4px #2C3532)' }} alt="test" />
+          <img
+            src={melKropyMenu}
+            style={{
+              width: '100%',
+              filter: 'drop-shadow( 15px 11px 4px #2C3532)'
+            }}
+            alt="test"
+          />
         </Grid>
         <Grid container spacing={2} item xs={10}>
           <Grid container item justifyContent="center" pt={2} xs={12}>
-              <img src={melKropyVisuel} style={{ width: '25%', objectFit: 'contain' }}/>
+            <img
+              src={melKropyVisuel}
+              style={{ width: '25%', objectFit: 'contain' }}
+            />
           </Grid>
           <Grid container item xs={12}>
-            <ImageList cols={matchDownMd ? 2 : 5 } gap={16}>
+            <ImageList cols={matchDownMd ? 2 : 5} gap={16}>
               {images.map((item) => (
-                <ImageListItem key={item.title} sx={{ cursor: 'pointer' }} onClick={handleClickOpen}>
+                <ImageListItem
+                  key={item.title}
+                  sx={{ cursor: 'pointer' }}
+                  onClick={handleClickOpen}
+                >
                   <img
                     src={`${item.img}?w=248&fit=crop&auto=format`}
                     srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
@@ -74,9 +152,9 @@ const MelKropy = (): JSX.Element => {
                   />
                 </ImageListItem>
               ))}
-          </ImageList>
+            </ImageList>
+          </Grid>
         </Grid>
-      </Grid>
       </Grid>
       <Dialog
         fullScreen={fullScreen}
@@ -85,26 +163,24 @@ const MelKropy = (): JSX.Element => {
         aria-labelledby="responsive-dialog-title"
       >
         <Toolbar>
-          <DialogTitle id="responsive-dialog-title">
-            {'test'}
-          </DialogTitle>
-            <IconButton
-              aria-label="close"
-              onClick={handleClose}
-              sx={{
-                position: 'absolute',
-                right: 8,
-                top: 8,
-                color: (theme) => theme.palette.grey[500]
-              }}
-            >
-              <CloseIcon />
-            </IconButton>
+          <DialogTitle id="responsive-dialog-title">{'test'}</DialogTitle>
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            sx={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500]
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
         </Toolbar>
         <DialogContent>
           <DialogContentText>
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
           </DialogContentText>
         </DialogContent>
       </Dialog>
